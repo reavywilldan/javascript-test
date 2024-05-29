aeb545df0c154b37a28a8dc0982bccc0b4e12f4a
reavy.willdan <r.wildan@baleomol.dev>
2024-05-29 13:35:01 +0700

    wip

--- js/analysis-plotter.js
+++ js/analysis-plotter.js
@@ -3,7 +3,7 @@
 /**
  * Plot result from the beam analysis calculation into a graph
  */
-function AnalysisPlotter (container) {
+function AnalysisPlotter(container) {
     this.container = container;
 }
 
@@ -13,7 +13,101 @@ AnalysisPlotter.prototype = {
      *
      * @param {Object{beam : Beam, load : float, equation: Function}}  The equation data
      */
-    plot : function (data) {
+    plot: function (data) {
         console.log('Plotting data : ', data);
+
+        this.destroyChart(this.container);
+
+        let xAxisLabel, yAxisLabel, interpolationMode;
+        switch (this.container) {
+            case 'deflection_plot':
+                xAxisLabel = 'Span (m)';
+                yAxisLabel = 'Deflection (kN)';
+                interpolationMode = 'monotone';
+                break;
+            case 'shear_force_plot':
+                xAxisLabel = 'Span (m)';
+                yAxisLabel = 'Shear Force (kN)';
+                break;
+            case 'bending_moment_plot':
+                xAxisLabel = 'Span (m)';
+                yAxisLabel = 'Bending Moment (kNm)';
+                interpolationMode = 'monotone';
+                break;
+            default:
+                return;
+        }
+
+        this.drawChart(data.equation.x, data.equation.y, xAxisLabel, yAxisLabel, interpolationMode);
     },
+
+    drawChart: function (x, y, xAxisLabel, yAxisLabel, interpolationMode) {
+        const ctx = document.getElementById(this.container).getContext('2d');
+        const condition = document.getElementById("condition").value;
+        const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
+        const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+
+        let segmentOption, borderColor = 'red', backgroundColor;
+
+        if (this.container === 'shear_force_plot') {
+            segmentOption = {
+                borderColor: ctx => skipped(ctx, 'rgba(0, 0, 0, 0)') || down(ctx, 'red'),
+            };
+            borderColor = 'rgba(0, 0, 0, 0)';
+        }
+
+        if (this.container === 'deflection_plot' && condition === "two-span-unequal") {
+            backgroundColor = 'rgba(0, 0, 0, 0)';
+        }
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: x,
+                datasets: [{
+                    data: y,
+                    borderColor: borderColor,
+                    borderWidth: 2,
+                    fill: true,
+                    pointRadius: 0,
+                    cubicInterpolationMode: interpolationMode || 'default',
+                    segment: segmentOption,
+                    backgroundColor: backgroundColor,
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                scales: {
+                    x: {
+                        title: {
+                            display: true,
+                            text: xAxisLabel
+                        },
+                        type: 'linear',
+                    },
+                    y: {
+                        title: {
+                            display: true,
+                            text: yAxisLabel
+                        },
+                        type: 'linear',
+                        grace: '10%'
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        });
+    },
+
+    destroyChart: function (chartId) {
+        const chartInstance = Chart.getChart(chartId);
+        if (chartInstance) {
+            chartInstance.destroy();
+        }
+    }
 };
--- js/beam-analysis.js
+++ js/beam-analysis.js
@@ -8,8 +8,8 @@
  * @param {String} name         Material name
  * @param {Object} properties   Material properties {EI : 0, GA : 0, ....}
  */
-function Material (name, properties) {
-    this.name       = name;
+function Material(name, properties) {
+    this.name = name;
     this.properties = properties;
 }
 
@@ -19,17 +19,17 @@ function Material (name, properties) {
  * @param {Number} secondarySpan        Beam secondary span length
  * @param {Material} material           Beam material object
  */
-function Beam (primarySpan, secondarySpan, material) {
-    this.primarySpan    = primarySpan;
-    this.secondarySpan  = secondarySpan;
-    this.material       = material;
+function Beam(primarySpan, secondarySpan, material) {
+    this.primarySpan = primarySpan;
+    this.secondarySpan = secondarySpan;
+    this.material = material;
 }
 
 /** ============================ Beam Analysis Class ============================ */
 
-function BeamAnalysis () {
+function BeamAnalysis() {
     this.options = {
-        condition : 'simply-supported'
+        condition: 'simply-supported'
     };
 
     this.analyzer = {
@@ -44,13 +44,13 @@ BeamAnalysis.prototype = {
      * @param {Beam} beam
      * @param {Number} load
      */
-    getDeflection : function (beam, load, condition) {
+    getDeflection: function (beam, load, condition) {
         var analyzer = this.analyzer[condition];
 
         if (analyzer) {
             return {
-                beam    : beam,
-                load    : load,
+                beam: beam,
+                load: load,
                 equation: analyzer.getDeflectionEquation(beam, load)
             };
         } else {
@@ -58,13 +58,13 @@ BeamAnalysis.prototype = {
         }
     },
 
-    getBendingMoment : function (beam, load, condition) {
+    getBendingMoment: function (beam, load, condition) {
         var analyzer = this.analyzer[condition];
 
         if (analyzer) {
             return {
-                beam    : beam,
-                load    : load,
+                beam: beam,
+                load: load,
                 equation: analyzer.getBendingMomentEquation(beam, load)
             };
         } else {
@@ -72,13 +72,13 @@ BeamAnalysis.prototype = {
         }
     },
 
-    getShearForce : function (beam, load, condition) {
+    getShearForce: function (beam, load, condition) {
         var analyzer = this.analyzer[condition];
 
         if (analyzer) {
             return {
-                beam    : beam,
-                load    : load,
+                beam: beam,
+                load: load,
                 equation: analyzer.getShearForceEquation(beam, load)
             };
         } else {
@@ -103,36 +103,58 @@ BeamAnalysis.analyzer = {};
  * @param {Number}  load    The applied load
  */
 BeamAnalysis.analyzer.simplySupported = function (beam, load) {
-    this.beam  = beam;
-    this.load   = load;
+    this.beam = beam;
+    this.load = load;
 };
 
 BeamAnalysis.analyzer.simplySupported.prototype = {
     getDeflectionEquation: function (beam, load) {
-        return function(x) {
-            return {
-                x : x,
-                y : null
-            };
+        const { primarySpan } = beam;
+        const { EI, j2 } = beam.material.properties;
+        const step = primarySpan / 10;
+        const xValues = [];
+        const vValues = [];
+
+        for (let i = 0; i <= primarySpan; i += step) {
+            const x = parseFloat(i.toFixed(1));
+            xValues.push(x);
+            const V = -((load * x) / (24 * EI)) * (Math.pow(primarySpan, 3) - 2 * primarySpan * Math.pow(x, 2) + Math.pow(x, 3)) * j2 * 1000;
+            vValues.push(parseFloat((V * 1e9).toFixed(1)));
         }
+
+        return { x: xValues, y: vValues };
     },
 
     getBendingMomentEquation: function (beam, load) {
-        return function(x) {
-            return {
-                x : x,
-                y : null
-            };
+        const { primarySpan } = beam;
+        const step = primarySpan / 10;
+        const xValues = [];
+        const vValues = [];
+
+        for (let i = 0; i <= primarySpan; i += step) {
+            const x = parseFloat(i.toFixed(1));
+            xValues.push(x);
+            const V = (load * x / 2) * (primarySpan - x) * -1;
+            vValues.push(parseFloat(V.toFixed(1)));
         }
+
+        return { x: xValues, y: vValues };
     },
 
     getShearForceEquation: function (beam, load) {
-        return function(x) {
-            return {
-                x : x,
-                y : null
-            };
+        const { primarySpan } = beam;
+        const step = primarySpan / 10;
+        const xValues = [];
+        const vValues = [];
+
+        for (let i = 0; i <= primarySpan; i += step) {
+            const x = parseFloat(i.toFixed(1));
+            xValues.push(x);
+            const V = load * (primarySpan / 2 - x);
+            vValues.push(parseFloat(V.toFixed(1)));
         }
+
+        return { x: xValues, y: vValues };
     }
 };
 
@@ -143,35 +165,94 @@ BeamAnalysis.analyzer.simplySupported.prototype = {
  * @param {Number}  load    The applied load
  */
 BeamAnalysis.analyzer.twoSpanUnequal = function (beam, load) {
-    this.beam  = beam;
-    this.load   = load;
+    this.beam = beam;
+    this.load = load;
 };
 
 BeamAnalysis.analyzer.twoSpanUnequal.prototype = {
     getDeflectionEquation: function (beam, load) {
-        return function(x) {
-            return {
-                x : x,
-                y : null
-            };
+        const { primarySpan, secondarySpan } = beam;
+        const { EI, j2 } = beam.material.properties;
+        const totalLength = primarySpan + secondarySpan;
+        const M1 = -((load * Math.pow(secondarySpan, 3)) + (load * Math.pow(primarySpan, 3))) / (8 * (primarySpan + secondarySpan));
+        const R1 = (M1 / primarySpan) + ((load * primarySpan) / 2);
+        const R3 = (M1 / secondarySpan) + ((load * secondarySpan) / 2);
+        const R2 = (load * primarySpan) + (load * secondarySpan) - R1 - R3;
+        const step = totalLength / 10;
+        const xValues = [];
+        const vValues = [];
+
+        for (let i = 0; i <= totalLength; i += step) {
+            const x = parseFloat(i.toFixed(1));
+            xValues.push(x);
+            const V = this.calculateDeflectionValue(x, { primarySpan, totalLength, EI, j2, R1, R2, load });
+            vValues.push(parseFloat(V.toFixed(1)));
+        }
+
+        return { x: xValues, y: vValues };
+    },
+
+    calculateDeflectionValue: function (x, { primarySpan, totalLength, EI, j2, R1, R2, load }) {
+        if (x < primarySpan) {
+            return ((x / (24 * EI)) * (4 * R1 * Math.pow(x, 2) - load * Math.pow(x, 3) + load * Math.pow(primarySpan, 3) - 4 * R1 * Math.pow(primarySpan, 2))) * j2 * 1000;
+        } else {
+            return (((R1 * x / 6) * (Math.pow(x, 2) - Math.pow(primarySpan, 2)) + (R2 * x / 6) * (Math.pow(x, 2) - 3 * primarySpan * x + 3 * Math.pow(primarySpan, 2)) - R2 * Math.pow(primarySpan, 3) / 6 - (load * x / 24) * (Math.pow(x, 3) - Math.pow(primarySpan, 3))) / EI) * j2 * 1000;
         }
     },
 
     getBendingMomentEquation: function (beam, load) {
-        return function(x) {
-            return {
-                x : x,
-                y : null
-            };
+        const { primarySpan, secondarySpan } = beam;
+        const totalLength = primarySpan + secondarySpan;
+        const M1 = -((load * Math.pow(secondarySpan, 3)) + (load * Math.pow(primarySpan, 3))) / (8 * (primarySpan + secondarySpan));
+        const R1 = (M1 / primarySpan) + ((load * primarySpan) / 2);
+        const R3 = (M1 / secondarySpan) + ((load * secondarySpan) / 2);
+        const R2 = (load * primarySpan) + (load * secondarySpan) - R1 - R3;
+        const step = totalLength / 10;
+        const xValues = [];
+        const vValues = [];
+
+        for (let i = 0; i <= totalLength; i += step) {
+            const x = parseFloat(i.toFixed(1));
+            xValues.push(x);
+            const V = this.calculateBendingMomentValue(x, { primarySpan, totalLength, R1, R2, load });
+            vValues.push(parseFloat(V.toFixed(1)));
         }
+
+        return { x: xValues, y: vValues };
+    },
+
+    calculateBendingMomentValue: function (x, { primarySpan, totalLength, R1, R2, load }) {
+        if (x === 0 || x === totalLength) return 0;
+        if (x < primarySpan) return -(R1 * x - 0.5 * load * Math.pow(x, 2));
+        if (x > primarySpan) return -(R1 * x + R2 * (x - primarySpan) - 0.5 * load * Math.pow(x, 2));
+        return -(R1 * primarySpan - 0.5 * load * Math.pow(primarySpan, 2));
     },
 
     getShearForceEquation: function (beam, load) {
-        return function(x) {
-            return {
-                x : x,
-                y : null
-            };
+        const { primarySpan, secondarySpan } = beam;
+        const totalLength = primarySpan + secondarySpan;
+        const M1 = -((load * Math.pow(secondarySpan, 3)) + (load * Math.pow(primarySpan, 3))) / (8 * (primarySpan + secondarySpan));
+        const R1 = (M1 / primarySpan) + ((load * primarySpan) / 2);
+        const R3 = (M1 / secondarySpan) + ((load * secondarySpan) / 2);
+        const R2 = (load * primarySpan) + (load * secondarySpan) - R1 - R3;
+        const step = totalLength / 10;
+        const xValues = [];
+        const vValues = [];
+
+        for (let i = 0; i <= totalLength; i += step) {
+            const x = parseFloat(i.toFixed(1));
+            xValues.push(x);
+            const V = this.calculateShearForceValue(x, { primarySpan, totalLength, R1, R2, load });
+            vValues.push(parseFloat(V.toFixed(1)));
         }
+
+        return { x: xValues, y: vValues };
+    },
+
+    calculateShearForceValue: function (x, { primarySpan, totalLength, R1, R2, load }) {
+        if (x === 0 || x === totalLength) return 0;
+        if (x < primarySpan) return -(R1 - load * x);
+        if (x > primarySpan) return -(R1 + R2 - load * x);
+        return -(R1 - load * primarySpan);
     }
 };
